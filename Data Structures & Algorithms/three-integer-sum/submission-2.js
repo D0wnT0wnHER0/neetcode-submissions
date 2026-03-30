2b7989ab49e4f0a423f5610e69c4ae10eaf1308d
D0wnT0wnHER0 <72611117+D0wnT0wnHER0@users.noreply.github.com>
2026-03-30 21:48:58 +0530

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        let s = new Set();
+        let n = nums.length;
+        for(let i=0; i<n; i++){
+            let ts = new Set();
+            for(let j=i+1; j<n; j++){
+                const t = -(nums[i] + nums[j])
+                if (ts.has(t)){
+                    const ans = [nums[i], nums[j], t];
+                    ans.sort((a,b) => a-b);
+                    s.add(JSON.stringify(ans));
+                }
+                ts.add(nums[j]);
+            }
+        }
+        return Array.from(s, JSON.parse);
+    }
+}
